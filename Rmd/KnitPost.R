022a276205335fdae83b3a8f1a43e122aa8285ab
Dason Kurkiewicz <dasonk@iastate.edu>
2012-10-07 21:18:46 -0500

    Added dinky and KnitPost

--- Rmd/KnitPost.R
+++ Rmd/KnitPost.R
@@ -1,12 +1,19 @@
 KnitPost <- function(input, base.url = "/") {
     require(knitr)
     require(lubridate)
+    
     opts_knit$set(base.url = base.url)
-    infilename <- sub(".Rmd$", "", basename(input))
-    fig.path <- paste0("../figs/", infilename, "/")
+    in.file <- sub(".Rmd$", "", basename(input))
+    fig.path <- paste0("../figs/", in.file, "/")
     opts_chunk$set(fig.path = fig.path)
     opts_chunk$set(fig.cap = "center")
+    
+    # Set the theme
+    thm = knit_theme$get("solarized-dark")
+    knit_theme$set(thm)
+    
     render_jekyll()
-    out.file <- paste0("../_posts/", today(), "-", infilename, ".md")
+    out.file <- paste0("../_posts/", today(), "-", in.file, ".md")
+    
     knit(input, output = out.file, envir = parent.frame())
 }
